b944a2d4bf1cf3f04f3a9b55c1da68297e41c7e4
alexjilavu29 <alexandrujilavu29@gmail.com>
2024-04-10 11:49:50 +0300

    Structure S8 File + Additional .csv Files (Course) - 2nd Commit

--- S8.py
+++ S8.py
@@ -11,6 +11,9 @@ plt.style.use('ggplot')
 
 matplotlib.rcParams['figure.figsize'] = (15,10)
 pd.options.mode.chained_assignment = None
+
+
+
 dfm = pd.read_csv("clienti_leasing.csv")
 print(dfm.head().to_string())
 print(dfm.describe())
@@ -52,7 +55,7 @@ while kurtosis determines the heaviness of the distribution tails'''
 '''Correlation coefficients are used to measure the strength of the relationship between two variables.
  Pearson correlation is the one most commonly used in statistics. This measures the strength and direction of a linear relationship between two variables.'''
 dfm_corr = df_int.corr()
-print(df_int.to_string())
+print(dfm_corr.to_string())
 
 y = dfm['REQUESTED_AMOUNT']
 plt.hist(y, bins=60)
@@ -81,10 +84,12 @@ between two variables or to form the most separated clusters'''
 '''distributia si relatia dintre coloana varsta si valoarea creditelor pentru cei care au cerut mai mult de 10000'''
 
 '''vad distributia creditelor in functie de varsta'''
-sns.pairplot(dfm[dfm['REQUESTED_AMOUNT'] > 100000], vars = ['AGE','VAL_CREDITS_RON'],
+sns.pairplot(dfm[dfm['REQUESTED_AMOUNT'] > 100000],
+             vars = ['AGE','VAL_CREDITS_RON'],
              hue = 'PRESCORING', diag_kind = 'hist')
 plt.show()
-sns.pairplot(dfm[dfm['REQUESTED_AMOUNT'] > 100000], vars = ['AGE', 'VAL_CREDITS_RON', 'REQUESTED_AMOUNT', 'VENIT_PER_YEAR_RON'],
+sns.pairplot(dfm[dfm['REQUESTED_AMOUNT'] > 100000],
+             vars = ['AGE', 'VAL_CREDITS_RON', 'REQUESTED_AMOUNT', 'VENIT_PER_YEAR_RON'],
              hue = 'PRESCORING', diag_kind = 'hist')
 plt.show()
 '''As noted above, a heatmap can be a good alternative to the scatter plot when there are a lot of data points that need to be plotted
@@ -102,6 +107,9 @@ plt.show()
 - grupați în funcție de coloana DATA valorile medii pentru coloanele venit anual, valoarea creditelor, suma din depozite și suma solicitată. Ordonați valorile în ordinea crescătoare a datei și reprezentați evoluția valorilor.
 - reprezentați sub formă de heatmap valorile medii ale sume solicitate în funcție de profesie și stare civilă. Tratați valorile null
 '''
+
+print(dfm['JOB'].value_counts())
+
 
 listOfJobs  = ['muncitor necalificat', 'profesor' , 'agricultor', 'asistent medical', 'barman', 'economist', 'inginer',
              'medic', 'pensionar']
