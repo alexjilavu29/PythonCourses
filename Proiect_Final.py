f504db5e21fc3f8ad2080c813f0e086320224c7a
alexjilavu29 <alexandrujilavu29@gmail.com>
2024-05-02 16:13:07 +0300

    Structure Proiect_Final.py File (+ Additional angajati.csv File) - 1st Commit

---
+++ Proiect_Final.py
@@ -0,0 +1,19 @@
+# Programare Python combinate sau individuale, minim 6 dintre următoarele facilităţi:
+# - utilizarea listelor şi a dicţionarelor, incluzând metode specifice acestora;
+# - utilizarea seturilor şi a tuplurilor, incluzând metode specifice acestora;
+# - definirea şi apelarea unor funcţii;
+# - utilizarea structurilor condiţionale;
+# - utilizarea structurilor repetitive;
+# - importul unei fişier csv sau json în pachetul pandas;
+# - accesarea datelor cu loc şi iloc;
+# - modificarea datelor în pachetul pandas;
+# - utilizarea funcţiilor de grup;
+# - tratarea valorilor lipsă;
+# - ştergerea de coloane şi înregistrări;
+# - prelucrări statistice, gruparea şi agregarea datalor în pachetul pandas;
+# - prelucrarea seturilor de date cu merge / join;
+# - reprezentare grafică a datelor cu pachetul matplotlib;
+# - utilizarea pachetului scikit-learn (clusterizare, regresie logistică);
+# - utilizarea pachetului statmodels (regresie multiplă).
+
+
