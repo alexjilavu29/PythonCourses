ad68e04a1d5e5ca186dbff40b4d1657b01419161
alexjilavu29 <alexandrujilavu29@gmail.com>
2024-05-02 16:52:58 +0300

    Structure Proiect_Final.py File (+ Additional functii_angajati.csv File) - 2nd Commit

--- Proiect_Final.py
+++ Proiect_Final.py
@@ -16,4 +16,21 @@
 # - utilizarea pachetului scikit-learn (clusterizare, regresie logistică);
 # - utilizarea pachetului statmodels (regresie multiplă).
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
+angajati = pd.read_csv("angajati.csv")
+print(angajati.head().to_string())
+
+# Adaugarea tuturor angajatilor din sectorul „Functii de conducere” in lista „conducere”
+conducere = angajati[angajati["Nume_Sector"] == "Funcții de conducere"]
+print(conducere)
+
+# Eliminarea din lista a tuturor angajatilor cu o vechime mai mica de 10 ani
+conducere = conducere[conducere["Vechime"] >= 10]
+
+# Preluarea tuturor id-urilor ca si chei si numelor angajatilor din conducere ca si valori intr-un dictionary
+dict_conducere = conducere.set_index("ID_angajat").to_dict()["Nume_prenume"]
+print(dict_conducere)
