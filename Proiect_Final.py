e8aecbbf32f1ce6c223603bc297911a87d965ee5
alexjilavu29 <alexandrujilavu29@gmail.com>
2024-05-03 19:56:30 +0300

    Add Final Features to Proiect_Final.py File -- 4th Commit

--- Proiect_Final.py
+++ Proiect_Final.py
@@ -43,24 +43,27 @@ print("\n")
 
 # Citirea fisierului „functii_angajati.csv”
 functii_angajati = pd.read_csv("functii_angajati.csv")
+print(functii_angajati.head().to_string())
+print("\n")
+
 
 # Asocierea id-urilor functiilor cu numele lor folosind un dictionar
 dict_functii = functii_angajati.set_index("ID_funcție").to_dict()["Funcția"]
+print(dict_functii)
+print("\n")
 
 # Crearea unei liste de tupluri unde fiecare angajat din conducere este asociat cu numele functiei sale
-# Mai intai, vom crea o lista de tupluri unde fiecare angajat din conducere este asociat cu id-ul functiei sale
-# Apoi, vom folosi dictionarul dict_functii pentru a inlocui id-urile cu numele functiilor
 angajati_functii = list(zip(conducere["ID_angajat"], conducere["ID_funcție"]))
 angajati_functii = [(dict_conducere[angajat], dict_functii[functie]) for angajat, functie in angajati_functii]
 print(angajati_functii)
 print("\n")
 
-# Determinarea sectoarelor diferite de activitate din dataframe-ul angajati folosind un set
+# Determinarea sectoarelor diferite de activitate din dataframe-ul angajati
 sectoare = set(angajati["Nume_Sector"])
 print(sectoare)
 print("\n")
 
-# Cate sectoare diferite de activitate se gasesc la CFR?
+# Determinarea numarului de sectoare diferite de activitate care se gasesc la CFR
 print(f"Numarul de sectoare diferite de activitate la CFR Romania este {len(sectoare)}.")
 print("\n")
 
@@ -70,11 +73,13 @@ print(numar_per_sector)
 print("\n")
 
 
-# Definirea unei functii pentru calcularea salariului mediu pentru fiecare functie, data fiind o intrare din tabela functti_angajati
-def salariu_mediu(row):
-    return (row["Salariu_min"] + row["Salariu_max"]) / 2
+# Definirea unei functii pentru calcularea salariului mediu pentru fiecare functie,
+# data fiind o intrare din tabela functii_angajati
+def salariu_mediu(intrare):
+    return (intrare["Salariu_min"] + intrare["Salariu_max"]) / 2
 
-# Aplicarea functiei definite anterior pe fiecare rand din tabela functii_angajati si adaugarea rezultatului in coloana „Salariu_mediu”
+# Aplicarea functiei definite anterior pe fiecare rand din tabela functii_angajati
+# si adaugarea rezultatului in coloana „Salariu_mediu”
 functii_angajati["Salariu_mediu"] = functii_angajati.apply(salariu_mediu, axis=1)
 print(functii_angajati.head().to_string())
 print("\n")
@@ -82,23 +87,26 @@ print("\n")
 # Definirea unei functii pentru rasplatirea angajatilor cu o anumita vechime
 def rasplata_vechime(row):
     procent_salariu = 0
-    if row["Vechime"] == 3:
-        procent_salariu += 0.05
-    elif row["Vechime"] == 5:
-        procent_salariu += 0.1
-    elif row["Vechime"] > 5:
-        i = (row["Vechime"] - (row["Vechime"] % 5)) / 5
+    if row["Vechime"] >= 10:
+        i = ((row["Vechime"] - (row["Vechime"] % 5)) / 5) - 1
+        procent_salariu += 0.15
         while i:
             procent_salariu += 0.05
             i -= 1
+    elif row["Vechime"] >= 5:
+        procent_salariu += 0.15
+    elif row["Vechime"] >= 3:
+        procent_salariu += 0.05
     return procent_salariu
 
-# Aplicarea functiei definite anterior pe fiecare rand din tabela angajati si adaugarea rezultatului in coloana „Rasplata_vechime”
+# Aplicarea functiei definite anterior pe fiecare rand din tabela angajati si
+# adaugarea rezultatului in coloana „Rasplata_vechime”
 angajati["Rasplata_vechime"] = angajati.apply(rasplata_vechime, axis=1)
 print(angajati.head().to_string())
 print("\n")
 
-# Adaugarea salariului mediu pentru fiecare angajat in tabela angajati in functie de coloana "Nume_Funcție" si salariul mediu din tabela functii_angajati
+# Adaugarea salariului mediu pentru fiecare angajat in tabela angajati in
+# functie de coloana "Nume_Funcție" si salariul mediu din tabela functii_angajati
 angajati["Salariu_mediu"] = angajati["Nume_Funcție"].map(functii_angajati.set_index("Funcția")["Salariu_mediu"])
 print(angajati.head().to_string())
 print("\n")
@@ -117,6 +125,7 @@ print("\n")
 print(angajati["Salariu_final"].describe())
 print("\n")
 
+
 # Crearea unui grafic care sa arate distributia salariilor finale
 plt.figure(figsize=(10, 6))
 sns.histplot(angajati["Salariu_final"], bins=30, color='skyblue')
@@ -125,9 +134,9 @@ plt.xlabel('Salariu Final')
 plt.ylabel('Numar de Angajati')
 plt.show()
 
-# Acelasi lucru folosind matplotlib
+# Crearea unui grafic care sa arate distributia salariilor finale folosind matplotlib
 plt.figure(figsize=(10, 6))
-plt.hist(angajati["Salariu_final"], bins=30, color='skyblue')
+plt.hist(angajati["Salariu_final"], bins=30, color='blue')
 plt.title('Distributia Salariilor Finale')
 plt.xlabel('Salariu Final')
 plt.ylabel('Numar de Angajati')
@@ -136,13 +145,20 @@ plt.show()
 # Localizarea angajatilor cu prenumele "Maria" care au salariul final mai mare de 10000
 # Prenumele se va obtine prin determinarea celui de-al doilea cuvant din variabila "Nume_prenume"
 angajati["Prenume"] = angajati["Nume_prenume"].apply(lambda x: x.split()[1])
-angajati_maria = angajati.loc[(angajati["Prenume"] == "Maria") & (angajati["Salariu_final"] > 10000), ["Nume_prenume", "Nume_Funcție", "Salariu_final"]]
+angajati_maria = angajati.loc[(angajati["Prenume"] == "Maria")
+                              & (angajati["Salariu_final"] > 10000),
+["Nume_prenume", "Nume_Funcție", "Salariu_final"]]
 print(angajati_maria.to_string())
 print("\n")
 
+# Preluarea înregistrărilor din tabela angajati_maria de pe pozitiile 1 si 3
+print(angajati_maria.iloc[[0, 2]])
+
+
 # Gruparea funcțiilor după intervalele de salariere din variabila "Clasificare Salariu Maxim"
 # si calcularea salariului mediu si mediana pentru fiecare grup
-statistici_salarii_per_interval = functii_angajati.groupby("Clasificare Salariu Maxim")["Salariu_mediu"].agg(["mean", "median"])
+statistici_salarii_per_interval = (functii_angajati.groupby("Clasificare Salariu Maxim")["Salariu_mediu"]
+                                   .agg(["mean", "median"]))
 print(statistici_salarii_per_interval)
 print("\n")
 
@@ -151,5 +167,5 @@ print("\n")
 # Vom folosi parametrul „on” pentru a specifica coloana dupa care se va realiza join-ul
 # Rezultatul va fi stocat in variabila „angajati_functii”
 angajati_functii = pd.merge(angajati, functii_angajati, on="ID_funcție")
-print(angajati_functii.to_string())
-print("\n")
+# Vom afisa doar coloanele "Num_prenume", "Nume_Funcție" si "Nume_Sector"
+print(angajati_functii[["Nume_prenume", "Funcția", "Nume_Sector"]].to_string())
